7de707db06e3c85e47197b0fb5cf15d06edaae35
Ollie <3martynov@gmail.com>
2021-04-07 20:32:33 -0400

    blog details added

---
+++ content/posts/todo.md
@@ -0,0 +1,5 @@
+---
+title: Latest Posts
+date: 2019-03-17T19:31:20.591Z
+body: 'empty'
+---
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { useRouter } from "next/router";
 
-const Post = () => {
+const Post = ({post}) => {
+    console.log(post)
     const router = useRouter();
     const { id } = router.query;
+    const {title, body} = post;
     return (
-        <div>
-            {id}
-        </div>
+        <article>
+          <h1>{title}</h1>
+          <div>
+              {id}
+              <div>{title}</div>
+              <div>{body}</div>
+          </div>
+        </article>
     )
 }
 
-export default Post
+export default Post;
+
+export async function getServerSideProps({params}){
+    const { id } = params;
+    console.log(id);
+    const post = await import(`../../content/posts/${id}.md`).catch(error => null);
+    console.log(post);
+    return { props: { post: post.attributes } };
+}
