f97d835344a71b7fa49d5aab80588cd3145a3b0b
Ollie <3martynov@gmail.com>
2021-04-18 16:55:53 -0400

    code clean up

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { attributes, html } from "../content/home.md";
-import styles from  "../styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
-export default class Home extends Component {
-  render() {
-    let { title, posts } = attributes;
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-          <title>Ollie Codes | Blog</title>
-        </Head>
-        <div className={styles.body}>
-          <div className={styles.hero}>
-            <h1>{title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: html }}/>
-          </div>
-          <ul>
-            {posts.reverse().map((post, k) => (
-              <div key={k}>
-                <Link href={post.link}>
-                  <h2 className={styles.link}>{post.name}</h2>
-                </Link>
-                <p>{post.description}</p>
-              </div>
-            ))}
-          </ul>
+const Home = ({ post }) => {
+  const { attributes, html } = post;
+  const { title, posts } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <title>Ollie Codes | Blog</title>
+      </Head>
+      <div className={styles.body}>
+        <div className={styles.hero}>
+          <h1>{title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>
-      </>
-    );
-  }
+        <ul>
+          {posts.reverse().map((post, k) => (
+            <div key={k}>
+              <Link href={post.link}>
+                <h2 className={styles.link}>{post.name}</h2>
+              </Link>
+              <p>{post.description}</p>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  console.log("building...");
+  const [blogpost] = await Promise.all([
+    import(`../content/home.md`).catch(() => null),
+  ]);
+  const { attributes = {}, html } = blogpost?.default ?? {};
+  return { props: { post: { attributes, html } } };
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,12 @@
 import React from "react";
-import fs from 'fs';
-import path from 'path';
-import Head from 'next/head';
-import styles from  "../../styles/Post.module.css";
-import 'prismjs/themes/prism-okaidia.css';
+import fs from "fs";
+import path from "path";
+import Head from "next/head";
+import styles from "../../styles/Post.module.css";
+import "prismjs/themes/prism-okaidia.css";
 
-
-
-const Post = ({post}) => {
-  const {attributes, html} = post;
+const Post = ({ post }) => {
+  const { attributes, html } = post;
 
   return (
     <>
@@ -26,22 +24,22 @@ const Post = ({post}) => {
 
 export default Post;
 
-export async function getStaticProps({params}){
-  console.log("building...")
-    const { id } = params;
-    console.log(id);
-    const [blogpost] = await Promise.all([
-      import(`../../content/posts/${id}.md`).catch(() => null)
-    ]);
-    const { attributes = {}, html } = blogpost?.default ?? {};
-    return { props: { post: {attributes, html} } };
+export async function getStaticProps({ params }) {
+  console.log("building...");
+  const { id } = params;
+  console.log(id);
+  const [blogpost] = await Promise.all([
+    import(`../../content/posts/${id}.md`).catch(() => null),
+  ]);
+  const { attributes = {}, html } = blogpost?.default ?? {};
+  return { props: { post: { attributes, html } } };
 }
 
 export async function getStaticPaths() {
-  console.log("building...")
+  console.log("building...");
   const paths = fs
-    .readdirSync(path.join(process.cwd(), 'content/posts'))
-    .map(blogName => {
+    .readdirSync(path.join(process.cwd(), "content/posts"))
+    .map((blogName) => {
       const trimmedName = blogName.substring(0, blogName.length - 3);
       return {
         params: { id: trimmedName },
@@ -52,4 +50,4 @@ export async function getStaticPaths() {
     paths,
     fallback: false, // constrols whether not predefined paths should be processed on demand, check for more info: https://nextjs.org/docs/basic-features/data-fetching#the-fallback-key-required
   };
-};
+}
