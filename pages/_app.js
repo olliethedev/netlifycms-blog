d9a9477fcf24578d0d632a8807e27b5127bf7135
olliethedev <3martynov@gmail.com>
2023-03-13 11:53:33 -0400

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/index.css";
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
+
+const G_TAG = "G-1JWLCFBV9T";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    </Head>
-    <Component {...pageProps} />
+      <>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${G_TAG}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${G_TAG}');
+        `}
+        </Script>
+      </>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <Component {...pageProps} />
     </>
   );
 }
